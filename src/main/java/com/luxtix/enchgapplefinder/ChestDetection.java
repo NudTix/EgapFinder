825aedcdaa3612951371e33efa491fc930e409e8
NudTix <72350452+NudTix@users.noreply.github.com>
2021-11-13 18:55:44 +0300

    Update ChestDetection.java

--- src/main/java/com/luxtix/enchgapplefinder/ChestDetection.java
+++ src/main/java/com/luxtix/enchgapplefinder/ChestDetection.java
@@ -1,75 +1,73 @@
-/*    */ package com.luxtix.enchgapplefinder;
-/*    */ 
-/*    */ import java.io.BufferedWriter;
-/*    */ import java.io.FileWriter;
-/*    */ import java.io.IOException;
-/*    */ import java.io.PrintWriter;
-/*    */ import net.minecraft.enchantment.EnchantmentHelper;
-/*    */ import net.minecraft.entity.Entity;
-/*    */ import net.minecraft.entity.item.EntityMinecartContainer;
-/*    */ import net.minecraft.entity.player.EntityPlayer;
-/*    */ import net.minecraft.init.Enchantments;
-/*    */ import net.minecraft.init.Items;
-/*    */ import net.minecraft.item.ItemStack;
-/*    */ import net.minecraft.tileentity.TileEntity;
-/*    */ import net.minecraft.tileentity.TileEntityLockableLoot;
-/*    */ import net.minecraft.world.World;
-/*    */ import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-/*    */ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-/*    */ import net.minecraftforge.fml.common.gameevent.TickEvent;
-/*    */ 
-/*    */ @EventBusSubscriber
-/*    */ public class ChestDetection {
-/*    */   @SubscribeEvent
-/*    */   public static void WorldTick(TickEvent.WorldTickEvent event) {
-/* 27 */     if (event.world.isRemote)
-/*    */       return; 
-/* 29 */     World world = event.world;
-/* 30 */     EntityPlayer player = null;
-/* 31 */     if (!world.playerEntities.isEmpty()) {
-/* 32 */       player = world.playerEntities.get(0);
-/* 36 */       for (TileEntity tile : world.loadedTileEntityList) {
-/* 37 */         if (tile instanceof TileEntityLockableLoot) {
-/* 38 */           TileEntityLockableLoot lockable = (TileEntityLockableLoot)tile;
-/* 39 */           if (lockable.getLootTable() != null) {
-/* 40 */             lockable.fillWithLoot(player);
-/* 41 */             for (int i = 0; i < lockable.getSizeInventory(); i++) {
-/* 42 */               ItemStack stack = lockable.getStackInSlot(i);
-/* 43 */               if (stack.getItem() == Items.GOLDEN_APPLE && stack.getItemDamage() == 1)
-/* 44 */                 writeToFile("Dungeon Chest with ench gapple at: " + lockable.getPos().getX() + " " + lockable.getPos().getY() + " " + lockable.getPos().getZ()); 
-/* 46 */               if (stack.getItem() == Items.ENCHANTED_BOOK && 
-/* 47 */                 EnchantmentHelper.getEnchantmentLevel(Enchantments.MENDING, stack) > 0)
-/* 48 */                 writeToFile("Dungeon Chest with Mending Book: " + lockable.getPos().getX() + " " + lockable.getPos().getY() + " " + lockable.getPos().getZ()); 
-/*    */             } 
-/*    */           } 
-/*    */         } 
-/*    */       } 
-/* 60 */       for (Entity entity : world.loadedEntityList) {
-/* 61 */         if (entity instanceof EntityMinecartContainer) {
-/* 62 */           EntityMinecartContainer cart = (EntityMinecartContainer)entity;
-/* 63 */           if (cart.getLootTable() != null) {
-/* 64 */             cart.addLoot(player);
-/* 65 */             for (int i = 0; i < cart.itemHandler.getSlots(); i++) {
-/* 66 */               ItemStack stack = cart.itemHandler.getStackInSlot(i);
-/* 67 */               if (stack.getItem() == Items.GOLDEN_APPLE && stack.getItemDamage() == 1)
-/* 68 */                 writeToFile("Minecart with ench gapple at: " + cart.posX + " " + cart.posY + " " + cart.posZ); 
-/* 70 */               if (stack.getItem() == Items.ENCHANTED_BOOK && 
-/* 71 */                 EnchantmentHelper.getEnchantmentLevel(Enchantments.MENDING, stack) > 0)
-/* 72 */                 writeToFile("Minecart with Mending at: " + cart.posX + " " + cart.posY + " " + cart.posZ); 
-/*    */             } 
-/*    */           } 
-/*    */         } 
-/*    */       } 
-/*    */     } 
-/*    */   }
-/*    */   
-/*    */   protected static void writeToFile(String coords) {
-/* 84 */     try(FileWriter fw = new FileWriter("LuxTix_egaps.txt", true);
-/* 85 */         BufferedWriter bw = new BufferedWriter(fw); 
-/* 86 */         PrintWriter out = new PrintWriter(bw)) {
-/* 87 */       out.println(coords);
-/* 88 */     } catch (IOException iOException) {}
-/*    */   }
-/*    */ }
+package com.luxtix.enchgapplefinder;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.EntityMinecartContainer;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Enchantments;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityLockableLoot;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 
+@EventBusSubscriber
+public class ChestDetection {
+  @SubscribeEvent
+  public static void WorldTick(TickEvent.WorldTickEvent event) {
+    if (event.world.field_72995_K)
+      return; 
+    World world = event.world;
+    EntityPlayer player = null;
+    if (!world.field_73010_i.isEmpty()) {
+      player = world.field_73010_i.get(0);
+      for (TileEntity tile : world.field_147482_g) {
+        if (tile instanceof TileEntityLockableLoot) {
+          TileEntityLockableLoot lockable = (TileEntityLockableLoot)tile;
+          if (lockable.func_184276_b() != null) {
+            lockable.func_184281_d(player);
+            for (int i = 0; i < lockable.func_70302_i_(); i++) {
+              ItemStack stack = lockable.func_70301_a(i);
+              if (stack.func_77973_b() == Items.field_151153_ao && stack.func_77952_i() == 1)
+                writeToFile("Dungeon Chest with ench gapple at: " + lockable.func_174877_v().func_177958_n() + " " + lockable.func_174877_v().func_177956_o() + " " + lockable.func_174877_v().func_177952_p()); 
+              if (stack.func_77973_b() == Items.field_151134_bR && 
+                EnchantmentHelper.func_77506_a(Enchantments.field_185296_A, stack) > 0)
+                writeToFile("Dungeon Chest with Mending Book: " + lockable.func_174877_v().func_177958_n() + " " + lockable.func_174877_v().func_177956_o() + " " + lockable.func_174877_v().func_177952_p()); 
+            } 
+          } 
+        } 
+      } 
+      for (Entity entity : world.field_72996_f) {
+        if (entity instanceof EntityMinecartContainer) {
+          EntityMinecartContainer cart = (EntityMinecartContainer)entity;
+          if (cart.func_184276_b() != null) {
+            cart.func_184288_f(player);
+            for (int i = 0; i < cart.itemHandler.getSlots(); i++) {
+              ItemStack stack = cart.itemHandler.getStackInSlot(i);
+              if (stack.func_77973_b() == Items.field_151153_ao && stack.func_77952_i() == 1)
+                writeToFile("Minecart with ench gapple at: " + cart.field_70165_t + " " + cart.field_70163_u + " " + cart.field_70161_v); 
+              if (stack.func_77973_b() == Items.field_151134_bR && 
+                EnchantmentHelper.func_77506_a(Enchantments.field_185296_A, stack) > 0)
+                writeToFile("Minecart with Mending at: " + cart.field_70165_t + " " + cart.field_70163_u + " " + cart.field_70161_v); 
+            } 
+          } 
+        } 
+      } 
+    } 
+  }
+  
+  protected static void writeToFile(String coords) {
+    try(FileWriter fw = new FileWriter("LuxTix_egaps.txt", true); 
+        BufferedWriter bw = new BufferedWriter(fw); 
+        PrintWriter out = new PrintWriter(bw)) {
+      out.println(coords);
+    } catch (IOException iOException) {}
+  }
+}

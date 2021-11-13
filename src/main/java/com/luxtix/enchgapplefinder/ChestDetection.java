b656c0720303256c8f5d99b39991059dd5733568
NudTix <72350452+NudTix@users.noreply.github.com>
2021-11-13 18:56:46 +0300

    Update ChestDetection.java

--- src/main/java/com/luxtix/enchgapplefinder/ChestDetection.java
+++ src/main/java/com/luxtix/enchgapplefinder/ChestDetection.java
@@ -22,40 +22,40 @@ import net.minecraftforge.fml.common.gameevent.TickEvent;
 public class ChestDetection {
   @SubscribeEvent
   public static void WorldTick(TickEvent.WorldTickEvent event) {
-    if (event.world.field_72995_K)
+    if (event.world.isRemote)
       return; 
     World world = event.world;
     EntityPlayer player = null;
-    if (!world.field_73010_i.isEmpty()) {
-      player = world.field_73010_i.get(0);
-      for (TileEntity tile : world.field_147482_g) {
+    if (!world.playerEntities.isEmpty()) {
+      player = world.playerEntities.get(0);
+      for (TileEntity tile : world.loadedTileEntityList) {
         if (tile instanceof TileEntityLockableLoot) {
           TileEntityLockableLoot lockable = (TileEntityLockableLoot)tile;
-          if (lockable.func_184276_b() != null) {
-            lockable.func_184281_d(player);
-            for (int i = 0; i < lockable.func_70302_i_(); i++) {
-              ItemStack stack = lockable.func_70301_a(i);
-              if (stack.func_77973_b() == Items.field_151153_ao && stack.func_77952_i() == 1)
-                writeToFile("Dungeon Chest with ench gapple at: " + lockable.func_174877_v().func_177958_n() + " " + lockable.func_174877_v().func_177956_o() + " " + lockable.func_174877_v().func_177952_p()); 
-              if (stack.func_77973_b() == Items.field_151134_bR && 
-                EnchantmentHelper.func_77506_a(Enchantments.field_185296_A, stack) > 0)
-                writeToFile("Dungeon Chest with Mending Book: " + lockable.func_174877_v().func_177958_n() + " " + lockable.func_174877_v().func_177956_o() + " " + lockable.func_174877_v().func_177952_p()); 
+          if (lockable.getLootTable() != null) {
+            lockable.fillWithLoot(player);
+            for (int i = 0; i < lockable.getSizeInventory(); i++) {
+              ItemStack stack = lockable.getStackInSlot(i);
+              if (stack.getItem() == Items.GOLDEN_APPLE && stack.getItemDamage() == 1)
+                writeToFile("Dungeon Chest with ench gapple at: " + lockable.getPos().getX() + " " + lockable.getPos().getY() + " " + lockable.getPos().getZ()); 
+              if (stack.getItem() == Items.ENCHANTED_BOOK && 
+                EnchantmentHelper.getEnchantmentLevel(Enchantments.MENDING, stack) > 0)
+                writeToFile("Dungeon Chest with Mending Book: " + lockable.getPos().getX() + " " + lockable.getPos().getY() + " " + lockable.getPos().getZ()); 
             } 
           } 
         } 
       } 
-      for (Entity entity : world.field_72996_f) {
+      for (Entity entity : world.loadedEntityList) {
         if (entity instanceof EntityMinecartContainer) {
           EntityMinecartContainer cart = (EntityMinecartContainer)entity;
-          if (cart.func_184276_b() != null) {
-            cart.func_184288_f(player);
+          if (cart.getLootTable() != null) {
+            cart.addLoot(player);
             for (int i = 0; i < cart.itemHandler.getSlots(); i++) {
               ItemStack stack = cart.itemHandler.getStackInSlot(i);
-              if (stack.func_77973_b() == Items.field_151153_ao && stack.func_77952_i() == 1)
-                writeToFile("Minecart with ench gapple at: " + cart.field_70165_t + " " + cart.field_70163_u + " " + cart.field_70161_v); 
-              if (stack.func_77973_b() == Items.field_151134_bR && 
-                EnchantmentHelper.func_77506_a(Enchantments.field_185296_A, stack) > 0)
-                writeToFile("Minecart with Mending at: " + cart.field_70165_t + " " + cart.field_70163_u + " " + cart.field_70161_v); 
+              if (stack.getItem() == Items.GOLDEN_APPLE && stack.getItemDamage() == 1)
+                writeToFile("Minecart with ench gapple at: " + cart.posX + " " + cart.posY + " " + cart.posZ); 
+              if (stack.getItem() == Items.ENCHANTED_BOOK && 
+                EnchantmentHelper.getEnchantmentLevel(Enchantments.MENDING, stack) > 0)
+                writeToFile("Minecart with Mending at: " + cart.posX + " " + cart.posY + " " + cart.posZ); 
             } 
           } 
         } 
